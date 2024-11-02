d9f4233d2f8eb784823e354640b8929b3436c00d
abhishek10ghosh <abhishek10ghosh@gmail.com>
2024-11-02 19:29:56 +0530

    inheritance

---
+++ Pillars/Inheritance/hierarchialInheritance.cpp
@@ -0,0 +1,35 @@
+// C++ program to implement Hierarchical Inheritance
+#include <iostream>
+using namespace std;
+
+// base class
+class Vehicle {
+public:
+    Vehicle() { cout << "This is a Vehicle\n"; }
+};
+
+// first sub class
+class Car : public Vehicle {
+public:
+    Car() { cout << "This Vehicle is Car\n"; }
+};
+
+// second sub class
+class Bus : public Vehicle {
+public:
+    Bus() { cout << "This Vehicle is Bus\n"; }
+};
+
+// main function
+int main()
+{
+    // Creating object of sub class will
+    // invoke the constructor of base class.
+    Car obj1;
+    Bus obj2;
+    return 0;
+//     This is a Vehicle
+// This Vehicle is Car
+// This is a Vehicle
+// This Vehicle is Bus
+}
---
+++ Pillars/Inheritance/hybridInheritance.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// base class
+class Vehicle {
+public:
+    Vehicle() { cout << "This is a Vehicle\n"; }
+};
+
+// base class
+class Fare {
+public:
+    Fare() { cout << "Fare of Vehicle\n"; }
+};
+
+// first sub class
+class Car : public Vehicle {
+  public:
+  Car() { cout << "This Vehical is a Car\n"; }
+};
+
+// second sub class
+class Bus : public Car, public Fare {
+  public:
+  Bus() { cout << "This Vehicle is a Bus with Fare\n"; }
+};
+
+// main function
+int main()
+{
+    // Creating object of sub class will
+    // invoke the constructor of base class.
+    Bus obj2;
+    return 0;
+}
+// This is a Vehicle
+// This Vehical is a Car
+// Fare of Vehicle
+// This Vehicle is a Bus with Fare
